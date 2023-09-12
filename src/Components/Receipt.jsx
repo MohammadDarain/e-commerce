ad2742e26aed8d2e5e907d6b989e6628514af641
mohddarain2 <mohddarain2@gmail.com>
2023-09-12 11:06:23 +0530

    print mode on

--- src/Components/Receipt.jsx
+++ src/Components/Receipt.jsx
@@ -6,6 +6,7 @@ import './Receipt.css'
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useCartContext } from '../Context/CartContext';
 import FormatPrice from '../Helper/FormatePrice';
+import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
 
 const Receipt = ({ receipt, SetReceipt }) => {
 
@@ -18,33 +19,34 @@ const Receipt = ({ receipt, SetReceipt }) => {
         SetReceipt(false)
     }
     return (
-        
-            <Dialog open={receipt}>
-                <div className='buttonCross'>
-                    <Button onClick={handleClose} style={{ backgroundColor: "white" }}> <CancelIcon style={{ fontSize: "29px" }} className='cancelIcon' /></Button>
-                </div>
-                <div className='mainReceipt'>
 
-                    <div className='giftBox'>
-                        <div className='faGiftIcon'>
-                            <Button style={{ backgroundColor: "white" }}><FaGift style={{ fontSize: "60px", color: "#f56a11" }} /></Button>
-                        </div>
-                        <div className='OrderPlaceHeading'>
-                            <h3 style={{ fontSize: "25px" }}>Order placed for  <FormatPrice price={shipping_fee + total_amount} /></h3>
-                            <h3 style={{ color: "#bf0019" }}>Your <span style={{ fontSize: "25px" }}>{total_item}</span>  item will be delivered by {date.toDateString()}</h3>
-                        </div>
+        <Dialog open={receipt}>
+        <div className='buttonCross'>
+        <Button onClick={window.print} style={{ backgroundColor: "white" ,color:"grey"}} title='Print'><LocalPrintshopIcon style={{fontSize:"30px"}}/></Button>
+                <Button onClick={handleClose}style={{ backgroundColor: "white"}}><CancelIcon style={{ fontSize: "29px" }} className='cancelIcon' /></Button>
+            </div>
+            <div className='mainReceipt'>
+
+                <div className='giftBox'>
+                    <div className='faGiftIcon'>
+                        <Button style={{ backgroundColor: "white" }}><FaGift style={{ fontSize: "60px", color: "#f56a11" }} /></Button>
                     </div>
-                    <h3 className='deliveryHeading'>Delivery Address</h3>
-                    <div className='giftBoxAddress'>
-                        <h3><span>Name</span> <b>:</b> {addressLocalStorage.username}</h3>
-                        <h3><span>Address</span> <b>:</b> {addressLocalStorage.address}</h3>
-                        <h3><span>Contact Number </span><b>:</b> {addressLocalStorage.number}</h3>
-                        <h3><span>PinCode </span><b>:</b> {addressLocalStorage.pincode}</h3>
+                    <div className='OrderPlaceHeading'>
+                        <h3 style={{ fontSize: "25px" }}>Order placed for  <FormatPrice price={shipping_fee + total_amount} /></h3>
+                        <h3 style={{ color: "#bf0019" }}>Your <span style={{ fontSize: "25px" }}>{total_item}</span>  item will be delivered by {date.toDateString()}</h3>
                     </div>
-                    <p style={{ fontSize: "12px", paddingTop: "20px" }}>Orders placed with '1 Day Delivery' option, will not have open-box delivery. 'Working condition' of the product will not be verified during delivery.</p>
                 </div>
-            </Dialog>
-        
+                <h3 className='deliveryHeading'>Delivery Address</h3>
+                <div className='giftBoxAddress'>
+                    <h3><span>Name</span> <b>:</b> {addressLocalStorage.username}</h3>
+                    <h3><span>Address</span> <b>:</b> {addressLocalStorage.address}</h3>
+                    <h3><span>Contact Number </span><b>:</b> {addressLocalStorage.number}</h3>
+                    <h3><span>PinCode </span><b>:</b> {addressLocalStorage.pincode}</h3>
+                </div>
+                <p style={{ fontSize: "12px", paddingTop: "20px" }}>Orders placed with '1 Day Delivery' option, will not have open-box delivery. 'Working condition' of the product will not be verified during delivery.</p>
+            </div>
+        </Dialog>
+
     )
 }
 
